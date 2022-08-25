daa555180ada9fb3e65a7f4ca0fcb406e0c4950c
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-08-25 22:39:57 +0530

    import java.util.*; class solution{     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int a=s.nextInt();         if(a>=0)         {             System.out.println("Positive Number");         }         else         {             System.out.println("Negative Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class solution{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a>=0)
+        {
+            System.out.println("Positive Number");
+        }
+        else
+        {
+            System.out.println("Negative Number");
+        }
+    }
+}
