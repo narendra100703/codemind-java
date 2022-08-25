e7773f37158c636e39215c5b14302448d63afb96
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-08-25 19:00:47 +0530

    import java.util.Scanner; class solution{     public static void main(String argS[])     {         Scanner scan=new Scanner(System.in);         String a=scan.nextLine();         System.out.print(a);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class solution{
+    public static void main(String argS[])
+    {
+        Scanner scan=new Scanner(System.in);
+        String a=scan.nextLine();
+        System.out.print(a);
+        
+    }
+}
