9a4603b200da672ddc4f3efa17f745ced2bcc76c
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-08-28 17:03:22 +0530

    import java.util.*; class solution{     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int c=0;         for(int i=0;i<n-1;i++)         {             if(a[i]<a[i+1])             {                 c++;             }         }         if(c==(n-1))         {             System.out.println("yes");         }         else         {             System.out.println("no");         }     } }

--- Ascending_array.java
+++ Ascending_array.java
@@ -5,16 +5,16 @@ class solution{
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int[] a=new int[n];
-        int  c=0;
         for(int i=0;i<n;i++)
         {
             a[i]=s.nextInt();
         }
+        int c=0;
         for(int i=0;i<n-1;i++)
         {
             if(a[i]<a[i+1])
             {
-                c+=1;
+                c++;
             }
         }
         if(c==(n-1))
