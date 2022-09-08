8fc83237933f51e808cd34ec93f87302df1cebbe
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-09-08 10:51:04 +0530

    import java.util.*; class solution {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         int c=s.nextInt();         System.out.println(a*b*c*512*2);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class solution
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        System.out.println(a*b*c*512*2);
+    }
+}
