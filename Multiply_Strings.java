0dab5e4b9150277d9aedeff4456f60edb61e2d3b
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-09-30 15:55:15 +0530

    import java.util.*; class solution {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         long a=s.nextLong();         long b=s.nextLong();         System.out.println(a*b);     } }

---
+++ Multiply_Strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class solution
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        long a=s.nextLong();
+        long b=s.nextLong();
+        System.out.println(a*b);
+    }
+}
