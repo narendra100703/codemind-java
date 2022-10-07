3a676f41b6feb1d12e11c7eced37637cd996caf0
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-10-07 10:52:52 +0530

    import java.util.Scanner; class Absolute_diffNum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int d=sc.nextInt();         int k=(int)Math.log10(n)+1;         int l=n%(int)Math.pow(10,d);         int f=n/(int)Math.pow(10,k-d);         System.out.print((int)Math.abs(f-l));     } }

---
+++ Absolute_difference_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Absolute_diffNum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d=sc.nextInt();
+        int k=(int)Math.log10(n)+1;
+        int l=n%(int)Math.pow(10,d);
+        int f=n/(int)Math.pow(10,k-d);
+        System.out.print((int)Math.abs(f-l));
+    }
+}
